085e1ffa5cf142cbe70eff091cb10445b623ebdb
ribincao <ribincao@gmail.com>
2021-07-29 15:16:09 +0800

    get bullet msg from bilibili

---
+++ BulletImBilibili/BulletIm.py
@@ -0,0 +1,28 @@
+import requests
+from pyquery import PyQuery as pq
+
+
+def get_bullet_from_bilibili(oid):
+    url = 'https://api.bilibili.com/x/v1/dm/list.so?oid=' + oid
+    headers = {'cookie': 'bsource=...'}  # header
+    result = requests.get(url, headers=headers)
+    result.encoding = 'utf-8'
+    html = pq(result.content)
+    bullets = html.find('d')  # find <d>
+
+    cnt = 0
+    ret = []
+    conditions = ['2021', '北京']
+    for bullet in bullets:
+        ret.append(bullet.text)
+        for condition in conditions:
+            if condition in bullet.text:
+                cnt += 1
+    # print(ret)
+    print(f"Num of all bullets: {len(bullets)}")
+    print(f"Num of bullets contain {conditions}: {cnt}")
+    print(str(round(cnt / len(bullets), 3)) + '%')
+
+
+if __name__ == '__main__':
+    get_bullet_from_bilibili('373384747')
